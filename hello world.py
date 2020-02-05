66fe9f49855506faaf4956b9a73265c5045da858
jakob <j.c.hild@email.msmary.edu>
2020-02-05 11:27:36 -0500

    Best Hello World Program

--- hello world.py
+++ hello world.py
@@ -1,1 +1,9 @@
-import random
+from random import randint, choice
+
+alpha = "ABCDEFHGIJKLMNOPQRSTUVWXYZ"
+
+res = ""
+while True:
+    if ''.join([choice(alpha) for i in range(randint(0, 12))]) == "HELLO WORLD":
+        break
+print(res)
